9d855fb4c6b295a10f21a98cc51c7ba9ab46c294
NikaEgorova <79790628+NikaEgorova@users.noreply.github.com>
2021-03-14 13:07:50 +0200

    Delete num1.6.py

--- lesson_4/num1.6.py
+++
@@ -1,8 +0,0 @@
-alex_salon = ['Audi R8 e tron', 'Mercedes Benz gle 400', 'Tesla Model S']
-oleg_salon = ['Лада Калина', 'Запорожець']
-len_alex_salon = len(alex_salon)
-len_oleg_salon = len(oleg_salon)
-if len_alex_salon < len_oleg_salon:
-    print("У Олега машин більше")
-else:
-    print("В Алексія машин більше")
