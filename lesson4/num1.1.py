83723935665c12357b82fef402b21a6eba98108e
NikaEgorovauser.email <evs9990@gmail.com>
2021-03-14 12:59:48 +0200

    Add new file

---
+++ lesson4/num1.1.py
@@ -0,0 +1,4 @@
+speed = 200
+distance = 600
+time = distance // speed
+print("Він проїде цю відстань за", time, "години")
---
+++ lesson4/num1.2.py
@@ -0,0 +1,5 @@
+speed = 200
+distance = 600
+time = distance // speed
+if time > 1:
+    print("Він їхав більше 1 години")
